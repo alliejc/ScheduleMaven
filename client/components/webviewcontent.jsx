ec1b6a368ff18bb0d0175191ecf0c0a980e20146
Alison Caldwell <alison.jane.c@gmail.com>
2017-03-25 19:33:01 -0700

    lint fixes

--- client/components/webviewcontent.jsx
+++ client/components/webviewcontent.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import 'react-pinterest/src/css/pinterest.css';
-
-
-class WebViewContent extends React.Component {
-
-  state = {
-    webUrl: 'https://www.pinterest.com/ZoeStone16/travel/',
-  };
-
-  render() {
-    if (this.props.webUrl) {
-      console.log(this.props.webUrl);
-      this.setState({ webUrl: this.props.webUrl });
-
-      return (
-        <div>
-          <iframe src={this.state.webUrl} name="targetframe" allowTransparency="true" scrolling="yes" frameBorder="0" />
-        </div>
-
-      );
-    }
-  }
-}
-export default WebViewContent;
+// import React from 'react';
+// import 'react-pinterest/src/css/pinterest.css';
+//
+//
+// class WebViewContent extends React.Component {
+//
+//   state = {
+//     webUrl: 'https://www.pinterest.com/ZoeStone16/travel/',
+//   };
+//
+//   render() {
+//     if (this.props.webUrl) {
+//       console.log(this.props.webUrl);
+//       this.setState({ webUrl: this.props.webUrl });
+//
+//       return (
+//         <div>
+//           <iframe src={this.state.webUrl} name="targetframe" allowTransparency="true" scrolling="yes" frameBorder="0" />
+//         </div>
+//
+//       );
+//     }
+//   }
+// }
+// export default WebViewContent;
 
