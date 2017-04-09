10a18172837457a4934667bb991027ad2da3a1ce
Allie Caldwell <alison.jane.c@gmail.com>
2017-04-09 14:09:04 -0700

    Added Pinterest pagination with Load More button

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+const style = {
+  position: 'absolute',
+  width: '100%',
+  height: '20px',
+  marginTop: '5%',
+  marginBottom: '5%',
+};
+
 const Footer = () => (
-  <footer className="page-footer">
-    <div className="footer-copyright">
+  <footer style={style}>
+    <div>
       <div className="container">
                 By using this app you are approving Schedule Maven to pin content on your behalf --
                 <a href="https://github.com/alliejc/React-Meteor-Pinterest-Scheduler/blob/master/README.md">Privacy Policy</a>
