43c4b2e586858fc5fe55e449e4436686fca041b2
Allie Caldwell <alison.jane.c@gmail.com>
2017-06-02 23:26:40 -0400

    Removed password login

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 import { AppBar } from 'material-ui';
 import { Accounts } from 'meteor/std:accounts-ui';
 
-Accounts.ui.config({
-  loginPath: '/login',
-  passwordSignupFields: 'USERNAME_ONLY',
-    // forceApprovalPrompt: false,
-    // onSignedInHook: () => FlowRouter.go('/'),
-    // onSignedOutHook: () => FlowRouter.go('/'),
-});
-
 class Header extends React.Component {
 
   render() {
